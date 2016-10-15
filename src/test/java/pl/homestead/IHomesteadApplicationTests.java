894d1a4534d84ab63d4fb8eff7867b465ff8f26a
Mateusz Lipiński <lipinski.mat@gmail.com>
2016-10-15 18:17:51 +0200

    initial 2

---
+++ src/test/java/pl/homestead/IHomesteadApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.homestead;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IHomesteadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
