1dd694ce8c3ab30dd956c04db3bc20a131267573
Mateusz Lipiński <lipinski.mat@gmail.com>
2016-10-15 18:34:15 +0200

    inital 4

--- src/main/java/pl/homestead/IHomesteadApplication.java
+++ src/main/java/pl/homestead/IHomesteadApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IHomesteadApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting application");
 		SpringApplication.run(IHomesteadApplication.class, args);
 	}
 }
