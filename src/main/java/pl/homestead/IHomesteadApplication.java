bb0d776f6bc34edd1efb10c1a007504aab898a25
Mateusz Lipiński <lipinski.mat@gmail.com>
2016-10-15 18:31:12 +0200

    initial 3

--- src/main/java/pl/homestead/IHomesteadApplication.java
+++ src/main/java/pl/homestead/IHomesteadApplication.java
@@ -1,9 +1,11 @@
 package pl.homestead;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class IHomesteadApplication {
 
 	public static void main(String[] args) {
